d79fcc4192e6d4ea4f325d6b11e6aca7749a8b61
TomKooyman <tom.kooyman@lightyear.one>
2021-01-21 08:40:55 +0100

    Continue day 18

--- y2019/Day 18/day18.py
+++ y2019/Day 18/day18.py
@@ -1,8 +1,9 @@
 import time
+import itertools
 
 def distanceto(point, d, alreadyfound, blockedby, inpath):
-    if point in upper.keys():
-        inpath.add(upper[point])
+    if point in doors.keys():
+        inpath.add(doors[point])
     blockedby[point] = inpath
     alreadyfound[point] = d
     d += 1
@@ -21,15 +22,15 @@ cave = []
 for line in f:
     cave.append(line.strip())
     
-lower = {}
-upper = {}
+keys = {}
+doors = {}
 neighbours = {}
 for y,line in enumerate(cave):
     for x,c in enumerate(line):
         if c.islower():
-            lower[c] = (x,y)
+            keys[c] = (x,y)
         elif c.isupper():
-            upper[(x,y)] = c
+            doors[(x,y)] = c
         elif c == '@':
             start = (x,y)
         n = set()
@@ -39,30 +40,62 @@ for y,line in enumerate(cave):
                     n.add((x+dx, y+dy))
             neighbours[(x,y)] = n
 
-distancetolower = {}
-blockedbylower = {}
-for l in lower:
-    distancetolower[l], blockedbylower[l] = distanceto(lower[l], 0, {}, {}, set())
+distancetokey = {}
+blockedbykey = {}
+for k in keys:
+    distancetokey[k], blockedbykey[k] = distanceto(keys[k], 0, {}, {}, set())
 distancetostart, blockedbystart = distanceto(start, 0, {}, {}, set())
 
 # =============================================================================
 # Debugging and trial-shit
 # =============================================================================
 
-# for line in cave:
-#     print(line)
-
 firstavailable = set()
-for l in lower:
-    # print(l, distancetostart[lower[l]])
-    if not blockedbystart[lower[l]]:
-        firstavailable.add(l)
-for l in lower:
-    for b in blockedbystart[lower[l]]:
-        if b.lower() in firstavailable:
-            print(l, blockedbystart[lower[l]])
+for k in keys:
+    if not blockedbystart[keys[k]]:
+        firstavailable.add(k)
+# for l in lower:
+#     print(l, blockedbystart[lower[l]])
+    # for b in blockedbystart[lower[l]]:
+    #     if b.lower() in firstavailable:
+    #         print(l, blockedbystart[lower[l]])
     
 print(firstavailable)
+
+
+# =============================================================================
+# Possible routes
+# =============================================================================
+
+s = time.time()
+
+routes = itertools.permutations(firstavailable, len(firstavailable))
+
+lengths = []
+for r in routes:
+    length = 0
+    for i, k in enumerate(r):
+        if i == 1:
+            length += distancetostart[keys[k]]
+        else:
+            length += distancetokey[k][keys[r[i-1]]]
+    lengths.append(length)
+    
+print(time.time()-s)
+    
+
+
+# print(routes)
+        
+
+
+
+
+# set.discard()
+
+
+
+
 
 # =============================================================================
 # Print map kinda thingy
