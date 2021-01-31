10791de186799c66ad20b41e21309d09b56d6157
TomKooyman <tom.kooyman@lightyear.one>
2021-01-31 16:34:55 +0100

    Removed some unused modules

--- Batterymodules.py
+++ Batterymodules.py
@@ -1,7 +1,7 @@
 import sys
-from PyQt5.QtCore    import Qt, QTimer
-from PyQt5.QtGui     import QDoubleValidator, QPainter, QColor, QBrush, QFont
-from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, QVBoxLayout, QTableWidget, QTableWidgetItem, QLineEdit
+from PyQt5.QtCore    import Qt
+from PyQt5.QtGui     import QDoubleValidator
+from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, QTableWidget, QTableWidgetItem, QLineEdit
 
 class GUI(QWidget):
     def __init__(self):
