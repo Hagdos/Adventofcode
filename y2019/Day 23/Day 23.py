d095ee201e3688e372d4dbb5f41e22f8ebd1d0f1
TomKooyman <tom.kooyman@lightyear.one>
2021-02-08 21:42:38 +0100

    Finished day 23

--- y2019/Day 23/Day 23.py
+++ y2019/Day 23/Day 23.py
@@ -2,26 +2,31 @@ import intcode as ic
 
 class Computer:
     def __init__(self, mem, address):
-        self.mem = mem
+        self.mem = mem.copy()
         self.address = address
         self.inputs = [address]
-        # self.instruction_pointer = 0
-        # self.relbase = 0
         self.inputcounter = 0
-        # self.loopcounter = 0
+        self.counters = [0, 0, 0, 0]
         self.finished = False
         
+    #Checks if there's new input for this computer; and adds it to the inputs
     def addinput(self, packages):
         if packages:
             for package in packages:
                 self.inputs.append(package)
+        #If there are no new packages, it will add -1 to the inputs-list once.
         else:
-            self.inputs.append(-1)
-                
-        
+            if self.inputs[-1] != -1:
+                self.inputs.append(-1)
+    
+    #Run checks if there are new inputs; if there are it runs the int-code with the existing counters and inputs. Otherwise it sets the output to 0; to prevent packages being added to the queues twice
     def run(self):
-        self.counters = [0, 0, self.inputcounter, 0]
-        self.mem, self.outputs, self.counters, self.finished = ic.runintcode(self.mem, self.inputs, self.counters)
+        if len(self.inputs) > self.inputcounter:
+            # self.counters = [self.instruction_pointer, self.relbase, self.inputcounter, 0]
+            self.mem, self.outputs, self.counters, self.finished = ic.runintcode(self.mem, self.inputs, self.counters)
+            self.inputcounter = self.counters[2]
+        else:
+            self.outputs = []
     
     def __repr__(self):
         return "Computer " + str(self.address)
@@ -35,31 +40,46 @@ mem = ic.codetomem(code)
 number_of_computers = 50
 computers = []
 packages = {}
-new_packages = {}
+NAT_Ys = []
+
+#Create the list of computers in the network; and fill the package dictionary with empty lists
 for n in range(number_of_computers):
     computers.append(Computer(mem, n))
     packages[computers[n]] = []
     
-computers[0].addinput([-1])
+while(True):
+    idle = True         #If not set to false; the computers are idle
     
-for _ in range(10):
+    #Loop over all computers; handle them 1 by 1
     for computer in computers:
+        #Add any packages in their queue to the inputs-list.
         computer.addinput(packages[computer])
+        #Clear the package queue
         packages[computer] = []
         computer.run()
-        print(computer)
-        print(computer.inputs)
-        print(computer.outputs)
-        print()
         
+        #Loop over the outputs of the computer that just ran        
         for p in range(0, len(computer.outputs), 3):
-            packages[computers[computer.outputs[p]]].extend(computer.outputs[p+1:p+3])
-            # computers[computer.outputs[p]].addinput(computer.outputs[p+1:p+3])  
+            #If the address is 255; the X-Y-values are stored in the NAT
+            if computer.outputs[p] == 255:
+                X = computer.outputs[p+1]
+                Y = computer.outputs[p+2]
+                NAT = [X, Y]
+            #Otherwise they are added to the package queue of the relevant computer
+            else:
+                idle = False            #Set idle to false; outputs are still happening this circle
+                packages[computers[computer.outputs[p]]].extend(computer.outputs[p+1:p+3])
+    
+    #If there are no more packages; the values in the NAT are sent to the first computer
+    if idle:
+        packages[computers[0]].extend(NAT)
+        if NAT[1] in NAT_Ys:                    #Y-value sent for the second time; the value we had to find.
+            break
+        NAT_Ys.append(Y)                        #Add Y-value that's sent out to the list; to check against next time around
 
     
+print("Answer to Part 1", NAT_Ys[0])
 
-# inputs = [41, 183922, 29]
-# counters = [0, 0, 0, 0]
-# mem, outputs, counters, finished = ic.runintcode(mem, inputs, counters)
+print("Answer to Part 2", NAT_Ys[-1])
 
-# print(outputs, finished)
+# 25305 is too high
