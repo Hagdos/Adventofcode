9c42fb3775f31898e3700f16220816afc2aa0f57
TomKooyman <tom.kooyman@lightyear.one>
2021-03-25 22:35:48 +0100

    Solved day 7

--- 2015/Day 7/Day 7.py
+++ 2015/Day 7/Day 7.py
@@ -1,10 +1,44 @@
+def solve(connections, cmd):
+    if isinstance(cmd, int) or cmd.isdigit():
+        return int(cmd)
+    
+    signal = connections[cmd]
+    if len(signal) == 1:
+        solution = solve(connections, signal[0])
+    elif signal[0] == 'NOT':
+        solution = (65535 - solve(connections, signal[1]))
+    elif signal[1] == 'LSHIFT':
+        solution = (solve(connections, signal[0]) << int(signal[2]))
+    elif signal[1] == 'RSHIFT':
+        solution = (solve(connections, signal[0]) >> int(signal[2]))
+    elif signal[1] == 'AND':
+        solution = (solve(connections, signal[0]) & solve(connections, signal[2]))
+    elif signal[1] == 'OR':
+        solution = (solve(connections, signal[0]) | solve(connections, signal[2]))
+    else:
+        print("Unknown command", connections[cmd])
+        
+    connections[cmd] = [solution]    
+    return solution
+
 commands = open('input.txt')
 connections = dict()
 
 for cmd in commands:
     cmd = cmd.strip().split(' -> ')
     connections[cmd[1]] = cmd[0].split(' ')
-    
-    
-#Iterative function; start at a and work down until you find a wire with a value on it.
 
+a = solve(connections, 'a')
+
+print('Answer to part 1:', a)
+
+commands = open('input.txt')
+connections = dict()
+
+for cmd in commands:
+    cmd = cmd.strip().split(' -> ')
+    connections[cmd[1]] = cmd[0].split(' ')
+
+connections['b'] = [a]
+
+print('Answer to part 2:', solve(connections, 'a'))
