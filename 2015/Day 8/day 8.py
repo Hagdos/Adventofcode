b57bba17a644f3264fcc58477b65d8980ed71b7e
TomKooyman <tom.kooyman@lightyear.one>
2021-03-26 22:24:15 +0100

    Solved day 8

--- 2015/Day 8/day 8.py
+++ 2015/Day 8/day 8.py
@@ -5,10 +5,30 @@ file = open('input.txt')
 codeCharacters = 0
 strCharacters = 0
 
-
 for wish in file:
     code = wish.strip()
     codeCharacters += len(code)
-    string = code.replace(r'\"', '"').strip('"')
+    string = code.strip('"')
+    string = string.replace(r'\"', '"')                     #Replace \" with "
+    string = string.replace(r'\\', '\\')                    #Replace \\ with \
+    string = re.sub(r'\\x[0-9a-f]{2}', 'a', string)         #Replace hexadecimals (\x..) with a
+    strCharacters += len(string)
     
-    # .replace(r'\\\\', "\\")
+print("Answer for part 1:", codeCharacters - strCharacters)
+
+# =============================================================================
+# Part 2
+# =============================================================================
+file = open('input.txt')
+
+codeCharacters = 0
+strCharacters = 0
+
+for wish in file:
+    string = wish.strip()
+    strCharacters += len(string)
+    code = string.replace('\\', '\\\\')
+    code = code.replace('"', '\\\"')
+    codeCharacters += len(code) + 2
+    
+print("Answer for part 2:", codeCharacters - strCharacters)
