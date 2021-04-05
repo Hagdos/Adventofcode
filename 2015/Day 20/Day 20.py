62927c4ad0ca16e8a82d4ca8d77c2a75ea26c0c0
TomKooyman <tom.kooyman@lightyear.one>
2021-04-05 17:11:35 +0200

    Solved day 20

--- 2015/Day 20/Day 20.py
+++ 2015/Day 20/Day 20.py
@@ -1,20 +1,20 @@
+#Function that finds all divisors of a number.
 def divisors(value):
-    divisors = set()
+    #1 and value are always divisors.
+    divisors = {1,value} 
+    #We want to loop over the values up to the square root of the number
     sqrt = round(value**(1/2) + 0.5)
-    for i in range(1, sqrt):
+    for i in range(2, sqrt):
         if value%i == 0:
             divisors.add(i)
             divisors.add(value//i)
-            
-    divisors.add(value)
-            
+                    
     return divisors
 
 def countPresents(house):
     return sum(divisors(house))*10
     
 import time
-
 start = time.time()
 
 threshold = 33100000
@@ -24,12 +24,34 @@ presents = 0
 while presents < threshold:
     house += 2      #Can add up with 2; unlikely that the first house w a lot of presents is an odd number
     presents = countPresents(house)
-
         
 print('Time: ', time.time()-start)
 print('House', house, 'has', presents, 'presents')
 
-776160
+# =============================================================================
+# Part 2
+# =============================================================================
 
+def divisorsPt2(value):
+    divisors = {value}
+    for i in range(2, 51):
+        if value%i == 0:
+            divisors.add(value//i)
+            
+    return divisors
 
+def countPresents2(value):
+    return sum(divisorsPt2(value))*11
 
+threshold = 33100000
+house = 0
+presents = 0
+
+while presents < threshold:
+    house += 2      
+    presents = countPresents2(house)
+    
+        
+print('Time: ', time.time()-start)
+print('House', house, 'has', presents, 'presents')
+    
