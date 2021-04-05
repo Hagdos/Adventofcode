b6c37bcbbd248c2152b14656db8f34d9ebca53bf
TomKooyman <tom.kooyman@lightyear.one>
2021-04-05 15:49:54 +0200

    Finished day 19

--- 2015/Day 19/Day 19.py
+++ 2015/Day 19/Day 19.py
@@ -20,58 +20,64 @@ for line in file:
 #         Part 1
 # =============================================================================
     
-# options = set()
+options = set()
     
-# for key in replacements.keys():
-#     positions = re.finditer(key, original)
-#     for position in positions:
-#         for replacement in replacements[key]:
-#             newMolecule = original[:position.span()[0]] +replacement + original[position.span()[1]:]
-#             options.add(newMolecule)
+for key in replacements.keys():
+    positions = re.finditer(key, original)
+    for position in positions:
+        for replacement in replacements[key]:
+            newMolecule = original[:position.span()[0]] +replacement + original[position.span()[1]:]
+            options.add(newMolecule)
         
-# print("Answer to part 1:", len(options))
+print("Answer to part 1:", len(options))
 
 # =============================================================================
 #  Part 2
 # =============================================================================
 
-def removeCa(molecule, steps):
-    for key, value in replacementsCa.items():
-        matches = re.findall(r'Ca', molecule)
-        molecule = re.sub(r'Ca', '', molecule)
-        steps += len(matches)
-    return molecule, steps
+def solve(molecule, steps):
+    molecules = {molecule: steps}
+    
+    noChanges = False
+    while noChanges == False:
+        noChanges = True
+        for molecule in list(molecules.keys()):
+            newFound = False
+            for key, rlist in replacements.items():
+                for replacement in rlist:
+                    matches = re.finditer(replacement, molecule)
+                    for m in matches:
+                        newFound = True
+                        newMolecule = molecule[:m.span()[0]] + key + molecule[m.span()[1]:] #Replace the match with the key.
+                        if newMolecule not in molecules.keys() or molecules[newMolecule] > molecules[molecule] + 1:
+                            molecules[newMolecule] = molecules[molecule] + 1 #Add 1 to the number of steps to reach this molecule
+            if newFound == True:
+                noChanges = False
+                del(molecules[molecule])
+            
+    return molecules
 
 molecule = original
 steps = 0
-# molecule, steps = removeCa(original[:], steps)
-
 molecules = {molecule: steps}
 
-for _ in range(3):
-    for molecule in list(molecules.keys()):
-        for key, rlist in replacements.items():
-            for replacement in rlist:
-                matches = re.finditer(replacement, molecule)
-                for m in matches:
-                    newMolecule = molecule[:m.span()[0]] + key + molecule[m.span()[1]:]
-                    if newMolecule not in molecules.keys() or molecules[newMolecule] > molecules[molecule] + 1:
-                        molecules[newMolecule] = molecules[molecule] + 1 #Add 1 to the number of steps to reach this molecule
-        del(molecules[molecule])
+while 'e' not in molecules.keys():
+    for molecule in molecules:
+        parts = re.split(r'(.*?Ar)', molecule)
+        parts = list(filter(None, parts))
+        
+        options = {'': molecules[molecule]}
+        
+        for i, p in enumerate(parts):
+            newOptions = {}
+            solutions = solve(p, 0)
+            for option in options.keys():
+                for solution in solutions:
+                    newOptions[option + solution] = options[option] + solutions[solution]
+                    # print(s)
+                    
+            options = newOptions
     
-    for molecule in list(molecules.keys()):
-        newMolecule, newSteps = removeCa(molecule, molecules[molecule])
-        if newMolecule != molecule:
-            if newMolecule not in molecules.keys() or molecules[newMolecule] > molecules[molecule] + 1:
-                molecules[newMolecule] = newSteps 
-            del(molecules[molecule])
-                
+    molecules = options
 
-# This runs out of hand very quickly, because there are a lot of double paths (replace 1 first, then 2, instead of 2)
-# print(molecules)
-    
-
-#Ar is always at the end; I guess we can split it up in pieces that end with Ar?
-#CRn is always at the beginning. Both CRn and Ar can't be created. So any solveable molecule has to start with CRn and end with Ar
-
-#Another thing to look at is to "generalize" molecule swaps. From P we can make Ca*(P*SiRnFAr*P*)*Ti* (and SiRnFAr can be used for more shit; but it will start and end with Ca and Ti)
+print("Answer to part 2:", molecules['e'])
