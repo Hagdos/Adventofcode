9629bf78a7feb0134fd47b924cbe9c2a2614d3e6
TomKooyman <tom.kooyman@lightyear.one>
2021-04-08 15:08:05 +0200

    Finished Day 22, part 2

--- 2015/Day 22/Day 22.py
+++ 2015/Day 22/Day 22.py
@@ -7,7 +7,7 @@ class Player:
         self.totalMana = 0
         self.game = game
         self.armour = 0
-
+        self.spells = []
     
     def createBoss(self):
         self.boss = Boss(self, self.game)
@@ -15,23 +15,24 @@ class Player:
     
     def turn(self):
         spell = self.userInput()
+        self.spells.append(spell)
         print()
         if spell == '1':
-            self.boss.hit(4)
             self.drainMana(53)
+            self.boss.hit(4)
         elif spell == '2':
+            self.drainMana(73)
             self.boss.hit(2)
             self.hp += 2
-            self.drainMana(73)
         elif spell == '3':
-            self.game.shieldTimer = 6
             self.drainMana(113)
+            self.game.shieldTimer = 6
         elif spell == '4':
-            self.game.poisonTimer = 6
             self.drainMana(173)
+            self.game.poisonTimer = 6
         elif spell == '5':
-            self.game.rechargeTimer = 5
             self.drainMana(229)
+            self.game.rechargeTimer = 5
         elif spell == 'E':
             self.hp = -10
         else:
@@ -46,18 +47,19 @@ class Player:
         print("Your turn. Pick a spell:")
         print()                 
         print('1) Magic missile (53 mana, 4 damage)')
-        print('2) Drain         (73 mana, 2 damage, heal 2')
-        print('3) Shield        (113 mana, +7 armour for 6 turns')
-        print('4) Poison        (173 mana, 3 damage for 6 turns')
-        print('5) Recharge      (229 mana, +101 mana for 5 turns')
+        print('2) Drain         (73 mana, 2 damage, heal 2)')
+        print('3) Shield        (113 mana, +7 armour for 6 turns)')
+        print('4) Poison        (173 mana, 3 damage for 6 turns)')
+        print('5) Recharge      (229 mana, +101 mana for 5 turns)')
         print()
         
         return input()
         
     def drainMana(self, mana):
         self.mana -= mana
-        if mana < 0:
-            self.hp = -1
+        # if self.mana < 0:
+        #     print("Mana too low:", self.mana, ". Game Over")
+        #     sys.exit()
         self.totalMana += mana
        
     def hit(self, damage):
@@ -139,48 +141,28 @@ class Game:
         if self.player.hp <= 0 or self.boss.hp <= 0:
             print(self.player)
             print("Total Mana spent:", self.player.totalMana)
+            print("Spells used: ", self.player.spells)
             print(self.boss)
             print("Game Over")
             sys.exit()
             return True
+        if self.player.mana < 0:
+            print("Mana too low")
+            print(self.player)
+            print("Total Mana spent:", self.player.totalMana)
+            print("Spells used: ", self.player.spells)
+            print(self.boss)
+            print("Game Over")
+            sys.exit()
+            
         else:
             return False
         
     def __repr__(self):
         return 'ShieldTimer: ' + str(self.shieldTimer) + '\nPoisonTimer: ' + str(self.poisonTimer) + '\nRechargeTimer: ' + str(self.rechargeTimer)
             
-
 # game = Game()
 game = Game(True)
 
-#Part 1: 900 (just from playing: Poison, shield, recharge, poison, missile (x4))
-
-# Part 2: 1080 is too low (running out of mana)
-# 1256 is too high...
-
-
-# Magic missile: 4 damage, 53 mana => 13.25 mana/damage
-# 4 damage/turn
-
-# Drain: 2 damage, 2 health, 73 mana => 36.5 mana/damage; or 18.25 mana/damage if health is counted too
-# 2 damage/turn
-
-# Shield: 7 armor for 6 turns. Effectively 21 health. 113 mana => 5.38 mana/health => Cheap!
-
-# Poison: 3 damage for 6 turns (18 damage), 173 mana => 9.61 mana/damage
-# 6 damage/turn (both turns)
-
-# Recharge: +101 mana for 5 turns (505 mana) costs 229 mana
-
-
-# -> Shield + Poison are the most effective. Shield is best but doesnt do damage. After that magic missile.
-
-# Boss = 51 Hp and 9 Damage
-# Player = 50 Hp
-
-# With always poison (3x); boss dies in 51/3 = 17 turns (8x player, 8x boss, 1x player w/o attack)
-# Last poison is "only" 5 turns; still 173/(5*6) = 5.8 mana/damage
-
-# In 8 turns; boss kills you (8*9) with 22 Hp to spare. 
-# One shield brings it down to -1 Hp, but takes an extra turn; so -10 Hp
-
+#Part 1: 900 (just from playing: poison, recharge, shield,  poison, missile (x4))
+#Part 2: 1216 (just from playing: poison, recharge, shield, poison, drain, recharge, poison, missile)
