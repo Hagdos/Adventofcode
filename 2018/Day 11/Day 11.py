96b3b42efa56e1e771610e23500b66a369ffa1a6
TomKooyman <tom.kooyman@lightyear.one>
2022-02-07 16:05:00 +0100

    Optimized code for day 11

--- 2018/Day 11/Day 11.py
+++ 2018/Day 11/Day 11.py
@@ -32,19 +32,21 @@ print(f'The answer to part 1: {ans1[0]},{ans1[1]}')
 # =============================================================================
 
 start = time.time()
-
 maxpower = -10
-for gridsize in range(1, 30):
-    for x in range(300-gridsize+1):
-        for y in range(300-gridsize+1):
-            power = 0
-            for dx in range(gridsize):
-                for dy in range(gridsize):
-                    power += cells[x+dx][y+dy]
+for x in range(300):
+    print(x)
+    for y in range(300):
+        power = 0
+        # Add the two edges and the corner for the new gridsize
+        for gridsize in range(min((300-x, 300-y, 30))):
+            power += sum(cells[x+gridsize][y:y+gridsize])
+            power += sum(c[y+gridsize] for c in cells[x:x+gridsize])
+            power += cells[x+gridsize][y+gridsize]
 
             if power > maxpower:
                 maxpower = power
-                ans2 = (x+1, y+1, gridsize)
+                ans2 = (x+1, y+1, gridsize+1)
+
 
 print(f'Time: {time.time()-start}')
 print(f'The answer to part 2: {ans2}')
