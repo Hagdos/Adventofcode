6c76e4e1c6d3517e3bdb21d7d455991b81469098
Tom Kooyman <tom.kooyman@lightyear.one>
2022-12-01 11:33:20 +0100

    Updated startnewpuzzle; added headers on request of Topaz

--- StartNewPuzzle.py
+++ StartNewPuzzle.py
@@ -4,8 +4,11 @@ from urllib.request import Request, urlopen
 import os
 import webbrowser
 
-year = input('Year: ')
-day = input('Day: ')
+# year = input('Year: ')
+# day = input('Day: ')
+
+year = 2022
+day = 1
 
 path = str(year) + '/Day ' + str(day)
 url = 'https://adventofcode.com/' + str(year) + '/day/' + str(day)
@@ -16,15 +19,16 @@ if os.path.isdir(path):
 else:
     # Read input data from HTTP
     inputrequest = Request(inputurl)
-    inputrequest.add_header('Cookie', 'session=53616c7465645f5f713fc7491ad2f15bd23ec8bc8bec78b5dc10c13afec0e9dd12e1bcee68342128c616e1e1670b76fb')
+    inputrequest.add_header('Cookie', 'session=53616c7465645f5fd1b146368a0b6a068fac2305df80be44e853f28e44defdf3b35cc7b49825406191bc263d8dab3b8370403074e32fe9e25b39a4496f974a0b')
+    inputrequest.add_header('github.com/Hagdos/Adventofcode by yourname@example.com')
     puzzleInput = urlopen(inputrequest).read()
-    
+
     # Write input data to input.txt
-    os.mkdir(path)
+    os.makedirs(path)
     inputFile = open(path + '/input.txt', 'wb')
     inputFile.write(puzzleInput)
     inputFile.close()
-    
+
     #Create python file
     pythonFile = open(path + '/Day ' + str(day) + '.py', 'w')
     pythonFile.write('file = open(\'input.txt\').readlines()\n\n')
@@ -34,7 +38,7 @@ else:
     pythonFile.write('print(\'The answer to part 1: \', ans1)\n')
     pythonFile.write('print(\'The answer to part 2: \', ans2)\n')
     pythonFile.close()
-    
+
     webbrowser.open(url)
-        
+
 # input('Done')
