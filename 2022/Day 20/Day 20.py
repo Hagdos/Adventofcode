775b23745f8d35cdbaabdaa8918b366806dcef17
TomKooyman <tom.kooyman@lightyear.one>
2022-12-20 09:06:26 +0100

    Cleaned up day 20

--- 2022/Day 20/Day 20.py
+++ 2022/Day 20/Day 20.py
@@ -14,78 +14,60 @@ class LL:
         else:
             print(l)
 
-file = open('input.txt').readlines()
+def readInput(filename, key):
+    file = open(filename).readlines()
 
-l = [LL(int(x)) for x in file]
-length = len(l)-1
+    linkedlist = [LL(int(x)*key) for x in file]
+    # length = len(l)-1
 
-for i in range(len(l)):
-    l[i-1].right = l[i]
-    l[i].left = l[i-1]
+    for i in range(len(linkedlist)):
+        linkedlist[i-1].right = linkedlist[i]
+        linkedlist[i].left = linkedlist[i-1]
 
-for i in l:
-    # l[0].printlist(l[0].n, [])
-    if i.n == 0:
-        start = i
-    # Take i out of the list (link their neighbours)
-    i.left.right = i.right
-    i.right.left = i.left
+        if linkedlist[i].n == 0:
+            start = linkedlist[i]
 
-    # move it i spaces to the right
-    right = i.right
-    for _ in range(i.n%length):
-        right = right.right
-
-    i.right = right
-    i.left = right.left
-    i.right.left = i
-    i.left.right = i
+    return linkedlist, start
 
 
+def findCoords(start):
+    ans = 0
+    for _ in range(3):
+        for _ in range(1000):
+            start = start.right
+        ans += start.n
+    return ans
 
-ans1 = ans2 = 0
-# Find answer 1:
-for _ in range(3):
-    for _ in range(1000):
-        start = start.right
-    ans1 += start.n
+
+def mix(l, cycles):
+    length = len(l)-1
+    for _ in range(cycles):
+        for i in l:
+            # Take i out of the list (link their neighbours)
+            i.left.right = i.right
+            i.right.left = i.left
+
+            # move it i spaces to the right
+            right = i.right
+            for _ in range(i.n%length):
+                right = right.right
+
+            i.right = right
+            i.left = right.left
+            i.right.left = i
+            i.left.right = i
+    return l
+
+
+l, start = readInput('input.txt', 1)
+l = mix(l, 1)
+ans1 = findCoords(start)
 
 print('The answer to part 1: ', ans1)
 
 key = 811589153
-l = [LL(int(x)*key) for x in file]
-length = len(l)-1
-
-for i in range(len(l)):
-    l[i-1].right = l[i]
-    l[i].left = l[i-1]
-
-for _ in range(10):
-    for i in l:
-        # l[0].printlist(l[0].n, [])
-        if i.n == 0:
-            start = i
-        # Take i out of the list (link their neighbours)
-        i.left.right = i.right
-        i.right.left = i.left
-
-        # move it i spaces to the right
-        right = i.right
-        for _ in range(i.n%length):
-            right = right.right
-
-        i.right = right
-        i.left = right.left
-        i.right.left = i
-        i.left.right = i
-
-
-# Find answer 1:
-for _ in range(3):
-    for _ in range(1000):
-        start = start.right
-    ans2 += start.n
+l, start = readInput('input.txt', key)
+l = mix(l, 10)
+ans2 = findCoords(start)
 
 print('The answer to part 2: ', ans2)
-
-#3577 is too low
