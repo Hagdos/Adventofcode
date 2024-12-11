7438deed45f22b172acf398ab13d8db323aadcc9
TomKooyman <tom.kooyman@lightyear.one>
2024-12-11 07:20:14 +0100

    Updated startingfile

--- StartNewPuzzle.py
+++ StartNewPuzzle.py
@@ -32,9 +32,9 @@ else:
     #Create python file
     pythonFile = open(path + '/Day ' + str(day) + '.py', 'w')
     pythonFile.write('file = open(\'input.txt\').readlines()\n\n')
-    pythonFile.write('data = [x.strip().split() for x in file]\n\n')
+    pythonFile.write('data = [x.strip().split() for x in file]\n')
+    pythonFile.write('print(data)\n')
     pythonFile.write('ans1 = ans2 = 0\n\n')
-    pythonFile.write('print(data)\n\n')
     pythonFile.write('print(\'The answer to part 1: \', ans1)\n')
     pythonFile.write('print(\'The answer to part 2: \', ans2)\n')
     pythonFile.close()
