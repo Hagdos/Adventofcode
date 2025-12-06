4e6176d15f5f5cf41cef474643cd9de50dde55f5
Hagdos <Email>
2025-12-06 07:38:34 +0100

    Improved Day 4; added BFS-sortof

--- 2025/Day 4/Day 4.py
+++ 2025/Day 4/Day 4.py
@@ -1,11 +1,18 @@
+import time
 import pyperclip
 import sys
 import os
+import heapq as hq
 sys.path.append(
     os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir, os.path.pardir)))
 import Tiles
 
-file = open('2025/Day 4/input.txt').read()
+start = time.time()
+
+file = open('2025/Day 4/input.txt').read() #0.71 seconds; 9784
+file = open('2025/Day 4/aoc-2025-day-4-challenge-1.txt').read() # 6.71 seconds
+file = open('2025/Day 4/aoc-2025-day-4-challenge-2.txt').read() # 121.27 seconds -> Down to 11
+# file = open('2025/Day 4/aoc-2025-day-4-challenge-3.txt').read() # 1985 seconds -> Down to 1.16
 
 grid = Tiles.Tiles(file)
 # print(grid)
@@ -15,34 +22,42 @@ heigth, width = grid.get_size()
 
 def remove_rolls(grid):
     removed_rolls = set()
+    ans1 = 0
 
     for x in range(heigth):
         for y in range(width):
-            if grid.get_tile((x,y)) == '@':
-                neighbours = grid.get_eight_neighbours((x,y))
-                occupied_neighbours = 0
-                for n in neighbours:
-                    if grid.get_tile(n) == '@':
-                        occupied_neighbours += 1
+            if grid.get_tile((x,y)) == '@' and (x,y):
+                if sum(grid.get_tile(n) == '@' for n in grid.get_eight_neighbours((x,y))) < 4:
+                    ans1 += 1
+                if (x,y) not in removed_rolls:
+                    removed_rolls.update(remove_rolls_BFS(grid, removed_rolls, (x, y)))
 
-                if occupied_neighbours < 4:
-                    removed_rolls.add((x,y))
 
-    for roll in removed_rolls:
-        grid.set_tile(roll, 'X')
     
-    return len(removed_rolls)
+    return len(removed_rolls), ans1
 
+def remove_rolls_BFS(grid, removed, start):
+    Queue = []
+    hq.heappush(Queue, start)  # Score, position
 
-removed_rolls = remove_rolls(grid)
-ans = removed_rolls
+    while Queue:
+        position = hq.heappop(Queue)
 
-print('The answer to part 1: ', ans)
-pyperclip.copy(ans)
+        if position in removed:
+            continue
 
-while removed_rolls:
-    removed_rolls = remove_rolls(grid)
-    ans += removed_rolls
+        if grid.get_tile(position) == '@':
+            if sum((grid.get_tile(n) == '@' and n not in removed) for n in grid.get_eight_neighbours(position)) < 4:
+                removed.add(position)
+                for n in grid.get_eight_neighbours(position):
+                    if grid.get_tile(n) == '@':
+                        hq.heappush(Queue, (n))
 
-print('The answer to part 2: ', ans)
-pyperclip.copy(ans)
+    return removed
+
+removed_rolls, ans1 = remove_rolls(grid)
+
+print('The answer to part 1: ', ans1)
+print('The answer to part 2: ', removed_rolls)
+
+print(f'This took {time.time()-start:.2f} seconds')
